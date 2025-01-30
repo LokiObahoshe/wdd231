9a36b91c4b438292cfa8e3fb181a2d47cd94ba38
LokiObahoshe <sondereggerbecca@gmail.com>
2025-01-29 21:47:33 -0600

    Fixed Issues with businesses displaying in home

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,3 +1,4 @@
+// Setting dates for timestamp
 if (document.querySelector('form')) {
     document.querySelector('form').addEventListener('submit', function (event) {
         const timestampInput = document.querySelector('#timestamp');
@@ -10,6 +11,7 @@ if (window.location.href.includes('thankyou.html')) {
     const currentUrl = window.location.href;
     //console.log(currentUrl);
 
+    // Replace and split methods used for URL string
     const everything = currentUrl.split('?');
     if (everything.length > 1) {
         let formData = everything[1].split('&');
@@ -26,6 +28,7 @@ if (window.location.href.includes('thankyou.html')) {
             return result;
         }
 
+        // Format for timestamp
         const timestamp = new Date(show("timestamp"));
         const formattedTimestamp = timestamp.toLocaleString('en-US', {
             weekday: 'long',
@@ -37,8 +40,10 @@ if (window.location.href.includes('thankyou.html')) {
             second: 'numeric'
         });
 
+        // Display submitted user info
         const showInfo = document.querySelector('#results');
         showInfo.innerHTML = `
+            <h2>Submitted User Information:</h2>
             <p>Appointment for ${show("first")} ${show("last")}</p>
             <p>Title: ${show("title")}</p>
             <p>Your Email: <a href="mailto:${show("email")}">${show("email")}</a></p>
@@ -48,4 +53,29 @@ if (window.location.href.includes('thankyou.html')) {
             <p>TimeStamp: ${formattedTimestamp}</p>
         `;
     }
+
 }
+
+//Dialog Trigger
+const courseDetails = document.getElementById('course-details');
+
+function showMembershipDetails(membershipType) {
+    courseDetails.innerHTML = `
+            <button id="closeModal">❌</button>
+            <h2>${membershipType} Membership Details</h2>
+            <p>Learn about the benefits and features of the ${membershipType} membership level.</p>
+        `;
+    courseDetails.showModal();
+
+    const closeModal = document.getElementById('closeModal');
+    closeModal.addEventListener("click", () => {
+        courseDetails.close();
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('nonprofitmembership').addEventListener('click', () => showMembershipDetails('Non-Profit'));
+    document.getElementById('bronzemembership').addEventListener('click', () => showMembershipDetails('Bronze'));
+    document.getElementById('silvermembership').addEventListener('click', () => showMembershipDetails('Silver'));
+    document.getElementById('goldmembership').addEventListener('click', () => showMembershipDetails('Gold'));
+});
