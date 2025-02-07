c17585ef17aac3241b41a1f65f9a85b30292e675
LokiObahoshe <sondereggerbecca@gmail.com>
2025-02-07 09:27:27 -0600

    W05 Assignment: Chamber Discover Page#1

---
+++ chamber/data/places.mjs
@@ -0,0 +1,50 @@
+export const places = [
+    {
+        "name": "Broken Arrow Historical Society Museum",
+        "address": "400 S Main St, Broken Arrow, OK 74012",
+        "description": "The Broken Arrow Historical Society Museum showcases the city's rich history with exhibits ranging from early settlers and Native American culture to the arrival of the railroad and Broken Arrow’s growth into a modern city. The museum is a great stop for history enthusiasts.",
+        "image": "images/historymuseum.webp"
+    },
+    {
+        "name": "Ray Harral Nature Park",
+        "address": "7101 S 3rd St, Broken Arrow, OK 74011",
+        "description": "Ray Harral Nature Park offers walking trails, serene ponds, and a peaceful environment to enjoy nature. The park is a perfect escape for hiking, bird watching, or just relaxing by the water. It’s a tranquil spot for nature lovers.",
+        "image": "images/rayharralpark.webp"
+    },
+    {
+        "name": "The Butterfly Garden at Rose District",
+        "address": "418 S Main St, Broken Arrow, OK 74012",
+        "description": "The Butterfly Garden in Broken Arrow's Rose District is a beautiful and peaceful oasis. This lovely garden is full of vibrant flowers and fluttering butterflies, providing a serene environment perfect for a leisurely stroll or a peaceful afternoon.",
+        "image": "images/butterflygarden.webp"
+    },
+    {
+        "name": "Broken Arrow Farmers Market",
+        "address": "418 S Main St, Broken Arrow, OK 74012 (Located in the Rose District)",
+        "description": "The Broken Arrow Farmers Market takes place every Saturday in the heart of the Rose District. It’s the place to go for fresh, locally-grown produce, handmade goods, artisan bread, and more. It’s a great spot to support local vendors and enjoy a lively, community atmosphere.",
+        "image": "images/farmersmarket.webp"
+    },
+    {
+        "name": "Military History Center",
+        "address": "112 N Main St, Broken Arrow, OK 74012",
+        "description": "The Military History Center offers a deep dive into military history, displaying an extensive collection of military memorabilia, artifacts, and exhibits that cover several wars and conflicts. The museum provides an educational and immersive experience for visitors of all ages.",
+        "image": "images/militaryhistorycenter.webp"
+    },
+    {
+        "name": "Broken Arrow Lanes",
+        "address": "2100 W New Orleans St, Broken Arrow, OK 74011",
+        "description": "A fun and family-friendly destination, Broken Arrow Lanes is a classic bowling alley offering lanes for all ages, a full-service restaurant, and arcade games. It’s a great place for an afternoon or evening of bowling, food, and fun with friends or family.",
+        "image": "images/lanes.webp"
+    },
+    {
+        "name": "Blue Bell Creameries Tour",
+        "address": "200 Blue Bell Dr, Broken Arrow, OK 74012",
+        "description": "The Blue Bell Creameries tour takes visitors behind the scenes to see how their delicious ice cream is made. After the tour, you can enjoy samples of their famous flavors. It’s a sweet and fun experience for ice cream lovers and families alike.",
+        "image": "images/bluebellcreameries.webp"
+    },
+    {
+        "name": "The Rose District",
+        "address": "Main St between 1st St & 5th St, Broken Arrow, OK 74012",
+        "description": "The Rose District is Broken Arrow’s vibrant downtown area, known for its charming boutiques, eclectic shops, trendy cafes, and art galleries. It's the perfect place to spend an afternoon shopping, dining, or exploring local art, and it offers plenty of events throughout the year.",
+        "image": "images/rosedistrict.webp"
+    }
+]
---
+++ chamber/scripts/discover.js
@@ -0,0 +1,49 @@
+import { places } from '../data/places.mjs'
+
+function placeCard(placeList) {
+    const displayplaces = document.getElementById('displayplaces');
+    displayplaces.innerHTML = '';
+
+    placeList.forEach((place, index) => {
+        const placeCard = document.createElement('div');
+
+        const placeFigure = document.createElement('figure');
+
+        const placeImage = document.createElement('img');
+        placeImage.setAttribute('src', place.image);
+        placeImage.setAttribute('alt', place.name);
+        // This conditional statement was added to
+        // reduce Largest Contentful Paint (LCP) that
+        // was removing points in performance
+        if (index === 0) {
+            placeImage.removeAttribute('loading');
+        } else {
+            placeImage.setAttribute('loading', 'lazy');
+        }
+        placeImage.setAttribute('width', '100%');
+        placeImage.setAttribute('height', 'auto');
+
+        placeFigure.appendChild(placeImage);
+        placeCard.appendChild(placeFigure);
+
+        const placeName = document.createElement('h2');
+        placeName.textContent = place.name;
+        placeCard.appendChild(placeName);
+
+        const placeAddress = document.createElement('address');
+        placeAddress.textContent = place.address;
+        placeCard.appendChild(placeAddress);
+
+        const placeDesc = document.createElement('p');
+        placeDesc.textContent = place.description;
+        placeCard.appendChild(placeDesc);
+
+        const placeButton = document.createElement('button');
+        placeButton.textContent = `Learn more`;
+        placeCard.appendChild(placeButton);
+
+        displayplaces.appendChild(placeCard);
+    });
+}
+
+placeCard(places);
