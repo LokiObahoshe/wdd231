4f088803f63184bb1e18e89a5d4882d0f6348c35
LokiObahoshe <sondereggerbecca@gmail.com>
2025-02-07 11:32:09 -0600

    W05 Assignment: Chamber Discover Page#2

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,5 +1,19 @@
 import { places } from '../data/places.mjs'
 
+// The Hamburger
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+// The Modified Dates
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = lastModified;
+
+// Display Places
 function placeCard(placeList) {
     const displayplaces = document.getElementById('displayplaces');
     displayplaces.innerHTML = '';
